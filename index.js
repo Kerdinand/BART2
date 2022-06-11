20a6465539e72d3a67a94147d927204bf63d1329
Kerdinand <ferdinandjonas.kloth@t-online.de>
2022-06-11 13:06:54 +0200

    color change

--- index.js
+++ index.js
@@ -1,34 +1,37 @@
+// DOM Elements
 const onewayElement = document.getElementById('oneway');
 const doubleElement = document.getElementById('double');
 const ArrDateLabel = document.getElementById('Arr-Date-Label');
 const ArrDate = document.getElementById('Arr-Date');
 
+// Begin Route Finder Color Switch
 const oneway = () => {
-	if (onewayElement.style.backgroundColor == 'gray') {
-		doubleElement.style.backgroundColor = 'gray';
-		onewayElement.style.backgroundColor = 'red';
+	if (onewayElement.style.backgroundColor == '#646973') {
+		doubleElement.style.backgroundColor = '#646973';
+		onewayElement.style.backgroundColor = '#EC0016';
 		ArrDate.style.display = 'none';
 		ArrDateLabel.style.display = 'none';
 	} else {
-		doubleElement.style.backgroundColor = 'red';
-		onewayElement.style.backgroundColor = 'gray';
+		doubleElement.style.backgroundColor = '#EC0016';
+		onewayElement.style.backgroundColor = '#646973';
 		ArrDate.style.display = 'block';
 		ArrDateLabel.style.display = 'block';
 	}
 };
 
 const double = () => {
-	if (doubleElement.style.backgroundColor == 'gray') {
-		doubleElement.style.backgroundColor = 'red';
-		onewayElement.style.backgroundColor = 'gray';
+	if (doubleElement.style.backgroundColor == '#646973') {
+		doubleElement.style.backgroundColor = '#EC0016';
+		onewayElement.style.backgroundColor = '#646973';
 		ArrDate.style.display = 'block';
 		ArrDateLabel.style.display = 'block';
 	} else {
-		doubleElement.style.backgroundColor = 'gray';
-		onewayElement.style.backgroundColor = 'red';
+		doubleElement.style.backgroundColor = '#646973';
+		onewayElement.style.backgroundColor = '#EC0016';
 		ArrDate.style.display = 'none';
 		ArrDateLabel.style.display = 'none';
 	}
 };
+// End Route Finder Color Switch
 
 console.log(oneway);
